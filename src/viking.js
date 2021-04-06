92321e15edc7e4b91f144e0e3ec54ea290f4f6e0
linesofcode01 <frcoding@outlook.com>
2021-04-06 13:37:15 -0700

    part1

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,30 @@
 // Soldier
-class Soldier {}
+class Soldier {
+constructor(health, strength){
+    this.health = health
+    this.strength = strength
+}
+
+attack = () =>{
+
+    return this.strength
+}
+
+receiveDamage = (damage) =>{
+    this.health -= damage
+}
+}
+let rambo = new Soldier(100, 95)
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength)
+        this.name = name
+    }
+
+}
+let thor = new Viking ("Thorrrrr!", 100, 90)
 
 // Saxon
 class Saxon {}
